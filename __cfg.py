0dc129c9374eeb0a22012fb0e7ee8fb38e558791
nubela <nubela@ctrleff.com>
2013-04-07 20:02:11 +0800

    updated cfg template to reflect new image settings

--- __cfg.py
+++ __cfg.py
@@ -1,4 +1,9 @@
 #general
+from base import S3
+
+UPLOAD_RELATIVE_ENDPOINT = "/media/uploads/"
+UPLOAD_FOLDER = "/home/nubela/Workspace/unifide-plop/support/uploads"
+ASSETS_FOLDER = "/home/nubela/Workspace/unifide-plop/base/assets/"
 SECRET_KEY = "b4ef3a73-5d52-11e2-9b58-14109feb3038"
 DOMAIN = "http://localhost:5001"
 MOCK_MODE = True
@@ -7,7 +12,9 @@ INSTALLED_PACKAGES = [
     "campaigns",
     "ecommerce",
     "orders",
-    ]
+    "base.items",
+    "base.org"
+]
 
 #mongodb
 MONGO_HOST = "localhost"
@@ -18,3 +25,12 @@ if MOCK_MODE:
 
 #mock stuff
 MOCK_DATE_RANGE_DAYS = 100
+
+#amazon s3 / upload folder
+AWS_ACCESS_KEY_ID = 'AKIAIYOWWMY2JWOT7YAA'
+AWS_SECRET_ACCESS_KEY = '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'
+S3_BUCKET_NAME = "ctrleff"
+S3_BUCKET_CHECK = False
+S3_KEY_NAME = "ctrleff"
+S3_LOCATION = S3.Location.SG
+CLOUDFRONT_URL = "http://d3rdt3fjkv9h3z.cloudfront.net/"
